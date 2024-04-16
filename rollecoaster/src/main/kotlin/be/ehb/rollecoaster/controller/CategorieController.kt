0c91a6ec6626ecf0dbdf698a4fd513ec8b09138a
amauryrauw <amaury.rauw@student.ehb.be>
2024-04-16 15:42:24 +0200

    feat: Add CRUD operations for Categorie entity in CategorieController
    Implemented HTTP methods for retrieving, adding, updating, and deleting Categorie entities in the CategorieController class, providing full CRUD functionality for the Categorie entity.

---
+++ rollecoaster/src/main/kotlin/be/ehb/rollecoaster/controller/CategorieController.kt
@@ -0,0 +1,42 @@
+package be.ehb.rollecoaster.controller
+
+import be.ehb.rollecoaster.model.Categorie
+import be.ehb.rollecoaster.service.CategorieService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/categories")
+class CategorieController(private val categorieService: CategorieService) {
+    @GetMapping
+    fun getAllCategories(): List<Categorie> {
+        return categorieService.getAllCategories()
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addCategorie(@RequestBody categorie: Categorie): Categorie {
+        return categorieService.addCategorie(categorie)
+    }
+
+    @PutMapping("/{id}")
+    fun updateCategorie(@PathVariable id: Long, @RequestBody categorie: Categorie): Categorie? {
+        return categorieService.updateCategorie(id, categorie)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCategorie(@PathVariable id: Long) {
+        categorieService.deleteCategorie(id)
+    }
+
+}
